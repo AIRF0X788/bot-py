861c92dd4c68231de762684f7ede01da7641b885
Guillaume L <gui2legrand2@gmail.com>
2023-11-10 14:41:25 +0100

    main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import json
 
 intents = discord.Intents.all()
+
 class CommandHistory:
     def __init__(self):
         self.history = {}
@@ -14,15 +15,22 @@ class CommandHistory:
             self.history[user_id].append(command)
         self.save_history()
 
-    def get_last_command(self, user_id):
+    def get_lc(self, user_id):
         if user_id in self.history and self.history[user_id]:
             return self.history[user_id][-1]
         return "Aucune commande précédente pour cet utilisateur."
 
-    def clear_history(self, user_id):
+    def get_ac(self, user_id):
+        if user_id in self.history and self.history[user_id]:
+            return self.history[user_id]
+        return "Aucune commande précédente pour cet utilisateur."
+
+    def ch(self, user_id):
         if user_id in self.history:
+            last_commands = self.history[user_id]
             self.history[user_id] = []
             self.save_history()
+            return last_commands
 
     def save_history(self):
         with open("command_history.json", "w") as file:
@@ -35,10 +43,7 @@ class CommandHistory:
         except FileNotFoundError:
             self.history = {}
 
-    def to_dict(self):
-        return self.history
-
-bot = commands.Bot(command_prefix='!', intents = intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 history_manager = CommandHistory()
 history_manager.load_history()
@@ -49,29 +54,39 @@ async def on_ready():
 
 @bot.event
 async def on_command(ctx):
-    if not ctx.author.bot:  # Vérifie que l'auteur de la commande n'est pas le bot lui-même
+    if not ctx.author.bot:
         user_id = ctx.author.id
         command = ctx.message.content
         history_manager.add_command(user_id, command)
 
 @bot.command()
-async def last_command(ctx):
+async def lc(ctx):
     user_id = ctx.author.id
-    last_command = history_manager.get_last_command(user_id)
-    await ctx.send(f"Dernière commande de l'utilisateur : {last_command}")
+    lc = history_manager.get_lc(user_id)
+    await ctx.send(f"Voici ta dernière commande utilisée : {lc}")
 
 @bot.command()
-async def clear_history(ctx):
+async def ac(ctx):
     user_id = ctx.author.id
-    history_manager.clear_history(user_id)
-    await ctx.send("L'historique de l'utilisateur a été effacé.")
-
-@bot.command()
-async def save_history(ctx):
-    if ctx.author.id: 
-        history_manager.save_history()
-        await ctx.send("L'historique a été sauvegardé.")
+    ac = history_manager.get_ac(user_id)
+    if isinstance(ac, list):
+        await ctx.send(f"Voici l'historique de toutes tes commandes : {', '.join(ac)}")
     else:
-        await ctx.send("Vous n'êtes pas autorisé à effectuer cette action.")
+        await ctx.send(ac)
+
+@bot.command()
+async def ch(ctx):
+    user_id = ctx.author.id
+    last_commands = history_manager.ch(user_id)
+    if last_commands:
+        await ctx.send("L'historique de l'utilisateur a été effacé.")
+        with open("command_history.json", "r") as file:
+            data = json.load(file)
+            del data[str(user_id)]
+        with open("command_history.json", "w") as file:
+            json.dump(data, file)
+    else:
+        await ctx.send("Aucune commande précédente pour cet utilisateur.")
+
 
 bot.run('MTE2NzM1OTk1MTg1ODA0OTA3NA.G2aXxo.rdwuUa0xkvwdD4Vbqua1_-bvcmOism-nubVp-A')
